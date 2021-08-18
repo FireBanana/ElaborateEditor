711c2bfa6b4a18c6ba2bfaca12e4d4f2582af53b
Owais Akhtar <free0run@gmail.com>
2021-08-18 17:01:49 +0500

    Added implementation of Imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+void ImGuiInit(GLFWwindow* window);
+void ImGuiLoop();
 
 int main(void)
 {
@@ -24,15 +32,42 @@ int main(void)
         return -1;
     }
 
+    ImGuiInit(window);
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
-
         glfwPollEvents();
+
+        ImGuiLoop();
+
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
     return 0;
 }
+
+void ImGuiInit(GLFWwindow* window)
+{
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+}
+
+void ImGuiLoop()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    bool demo = true;
+    ImGui::ShowDemoWindow(&demo);
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
